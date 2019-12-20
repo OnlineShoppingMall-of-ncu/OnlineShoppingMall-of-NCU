754948aa5abe856c80b26644b82a6da55a023be7
cronaldo722 <2523179247@qq.com>
2019-12-20 18:54:13 +0800

    实现后台管理登录

---
+++ src/main/java/cn/ncu/edu/onlineshopmall/Controller/admin/LoginController.java
@@ -0,0 +1,59 @@
+package cn.ncu.edu.onlineshopmall.Controller.admin;
+
+import cn.ncu.edu.onlineshopmall.Service.UserService;
+import cn.ncu.edu.onlineshopmall.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+    @Autowired
+    UserService userService ;
+
+    @RequestMapping("/login")
+    public String adminLogin() {
+        return "admin-login";
+    }
+
+
+    @RequestMapping("/success")
+    public String loginsuccess(){
+        return "admin-index";
+    }
+
+
+
+    @RequestMapping(value = "/confirmLogin",method = RequestMethod.POST)
+    public String confirmLogin(@RequestParam("username")String username, @RequestParam("password") String password, Model model, HttpServletRequest request){
+
+        User LoginUser = userService.findUserByUsername(username);
+        if (LoginUser == null||!LoginUser.equals(password)){
+            model.addAttribute("errorMsg", "用户名或密码错误");
+            return "admin-login";
+        }else
+            {
+            HttpSession session = request.getSession();
+            session.setAttribute("LoginUser",LoginUser);
+            return "redirect:/admin/success";
+        }
+    }
+
+
+
+
+    @RequestMapping("/logout")
+    public String adminLogout(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.removeAttribute("admin");
+        return "redirect:/admin/login";
+    }
+}
